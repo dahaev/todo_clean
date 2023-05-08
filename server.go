e42f95e77b2471494654038c5cee2ece0cd0eeaf
dahaev <105097809+dahaev@users.noreply.github.com>
2023-05-08 22:36:52 +0300

    test_commit

--- server.go
+++ server.go
@@ -22,6 +22,8 @@ func (s *Server) Run(port string, handler http.Handler) error {
 	return s.httpServer.ListenAndServe()
 }
 
+// Shutting down server
+
 func (s *Server) ShutDown(ctx context.Context) error {
 	return s.httpServer.Shutdown(ctx)
 }
